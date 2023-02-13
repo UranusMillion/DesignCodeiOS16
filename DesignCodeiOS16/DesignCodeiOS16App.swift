59c727c5b7a916e7eed19e6223feba672d102f1d
Leon <rock_sounding@hotmail.com>
2023-02-13 14:48:00 +0800

    Initial Commit

---
+++ DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by leonlee on 2023/2/13.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
